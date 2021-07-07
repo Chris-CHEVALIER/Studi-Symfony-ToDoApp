049e76a49eb0bf9c30cef254428a041964524da4
Chevalier <chevalierchris34@gmail.com>
2021-07-07 18:52:46 +0200

    Task CRUD

--- src/Controller/TodoListController.php
+++ src/Controller/TodoListController.php
@@ -23,6 +23,7 @@ class TodoListController extends AbstractController {
             $em = $this->getDoctrine()->getManager();
             $em->persist($todoList);
             $em->flush();
+            return $this->redirectToRoute("read_all");
         }
 
         return $this->render("todo_list/create.html.twig", [
@@ -51,11 +52,22 @@ class TodoListController extends AbstractController {
 
         if ($form->isSubmitted() && $form->isValid()) {
             $this->getDoctrine()->getManager()->flush();
+            return $this->redirectToRoute("read_all");
         }
 
         return $this->render("todo_list/create.html.twig", [
             "form" => $form->createView(),
             "list" => $list
         ]);
+    }
+
+    /**
+     * @Route("/delete-list/{id}", name="delete_list")
+     */
+    public function delete(TodoList $list): Response {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($list);
+        $em->flush();
+        return $this->redirectToRoute("read_all");
     }
 }
